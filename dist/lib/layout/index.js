b00bef1119c17e689355b29dd723f687e379c412
alanxu <alanxu@cppib.com>
2018-04-23 13:07:27 -0400

    Remove dist

--- dist/lib/layout/index.js
+++
@@ -1,3 +0,0 @@
-export * from './layout.module';
-export * from './header/header.component';
-//# sourceMappingURL=index.js.map
--- dist/lib/tilt-common.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ApplicationConfig, NavigationConfig } from './application';
-export declare const APPLICATION_CONFIG: ApplicationConfig;
-export declare const NAVIGATION_CONFIG: NavigationConfig;
-export declare class TiltCommonModule {
-}
