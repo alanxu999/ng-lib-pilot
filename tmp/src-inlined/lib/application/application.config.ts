2fd54d782e723d9f92ac859c800e51873d6495a4
alanxu <alanxu@cppib.com>
2018-04-23 17:25:08 -0400

    Delete temp

--- tmp/src-inlined/lib/application/application.config.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const APPLICATION_CONFIG_TOKEN = new InjectionToken<ApplicationConfig>('APPLICATION_CONFIG_TOKEN');
-
-export interface ApplicationConfig {
-    name: string;
-    label: string;
-}
